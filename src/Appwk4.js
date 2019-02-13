bdfcee54ffb5eb8573eb8044a57550000e2a00be
Justin Cheng <Yusi.Cheng.0509@gmail.com>
2019-02-13 12:46:35 -0800

    previous backup

---
+++ src/Appwk4.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ArticleTable from './ArticleTable';
+import articles from './data/articles.json';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <ArticleTable articles={articles} />
+            </div>
+        );
+    }
+}
+
+export default App;
