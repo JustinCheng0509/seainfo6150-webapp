34547c1892914b0bff25e6330b8e0846fc67058d
JCheng0509 <Yusi.Cheng.0509@gmail.com>
2019-02-27 17:02:39 -0800

    week 3 changes
    removed i tags

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ class App extends Component {
     <em> <strong>"Although it is not one of the most difficult things we have ever moved,"</strong></em> says Douglas Phelps, who oversaw the relocation of the torch, <em><strong>"it is certainly the most important."</strong></em></p>
     </section>
     <section>
-    <p>&emsp;Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of <i>"Outstanding Universal Value."</i>
+    <p>&emsp;Not only is the Statue of Liberty the symbol of New York City, she is also a UNESCO World Heritage site under the designation of "Outstanding Universal Value."
     Her full name is Liberty Enlightening the World, and she was designed by Frederic Auguste Bartholdi and built by Gustave Eiffel -- yes, the same guy who built Paris's Eiffel Tower.
     Lady Liberty was a gift from France to the United States to celebrate the centennial of their nation's independence -- although, somewhat awkwardly, she arrived a decade too late. Nevertheless, President Grover Cleveland formally unveiled her in 1886.</p>
     </section>
     <section>
-      <p>Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.<em><strong>"The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," </strong></em>says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit was bolstered by the decision to inscribe Emma Lazarus' poem <i>"The New Colossus" </i>on the statue's pedestal, which gave Lady Liberty one of her nicknames, <i>"Mother of Exiles."</i>The most famous lines in the poem are:<br/><em><strong>"Give me your tired, your poor / Your huddled masses yearning to breathe free."</strong></em></p>
+      <p>Since then, the statue has served as a symbol of hope and possibility for many people who entered the United States as immigrants via neighboring Ellis Island.<em><strong>"The original torch is a beautiful object of art and exemplifies the American ideals of freedom and democracy," </strong></em>says John Piltzecker, superintendent of the Statue of Liberty National Monument. That symbolic spirit was bolstered by the decision to inscribe Emma Lazarus' poem "The New Colossus" on the statue's pedestal, which gave Lady Liberty one of her nicknames, "Mother of Exiles."The most famous lines in the poem are:<br/><em><strong>"Give me your tired, your poor / Your huddled masses yearning to breathe free."</strong></em></p>
     </section>
     <section>
-    <p>&emsp;In <time datetime="2017">2017</time>, nearly <var>4.5</var> million people visited Liberty Island. When completed, the Statue of Liberty Museum will be <var>26,000 </var>square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world. The <i>"inspiration gallery,"</i> which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.</p>
+    <p>&emsp;In <time datetime="2017">2017</time>, nearly <var>4.5</var> million people visited Liberty Island. When completed, the Statue of Liberty Museum will be <var>26,000 </var>square feet of history, inspiration and deeper analysis of the statue's symbolism both in the US and around the world. The "inspiration gallery," which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower Manhattan, including the World Trade Center.</p>
     </section>
 </div>;   
     return (
