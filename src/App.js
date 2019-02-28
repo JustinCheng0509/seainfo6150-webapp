f6936a7ac36a732de2e15169b8bffc8ec1999210
JCheng0509 <Yusi.Cheng.0509@gmail.com>
2019-02-27 16:11:45 -0800

    Week4 app update
    week4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import ArticleList from "./ArticleList/ArticleList";
+import ArticleTable from './ArticleTable';
 import articles from './data/articles.json';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ArticleList articles={articles} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <ArticleTable articles={articles} />
+            </div>
+        );
+    }
 }
 
 export default App;
